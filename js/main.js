43505489632b9e3674f4a582c09f90c6ff2412b4
OsamaPaiola <paiolaosama@gmail.com>
2020-11-10 15:24:27 +0100

    add

--- js/main.js
+++ js/main.js
@@ -2,5 +2,23 @@
 
 const app = new Vue({
   el:'#app',
-
+  data:{
+    items: [
+      { message: 'Fare colazione' },
+      { message: 'Palestra' },
+      { message: 'Doccia' },
+      { message: 'Corso Boolean' },
+    ],
+    text:'',
+    // item: { message: '' }
+  },
+  methods:{
+    add(){
+      this.items.push({ message: this.text});
+      this.text='';
+    },
+    del(index){
+      this.items.$remove(index);
+    }
+  },
 });
